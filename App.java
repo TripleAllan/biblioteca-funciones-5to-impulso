05a6c03bc8e16a5a2c31d43ccd7b5a2c2f905f73
Allan <146860720+TripleAllan@users.noreply.github.com>
2024-09-24 23:20:36 +0000

    Hice Division

--- App.java
+++ App.java
@@ -23,7 +23,10 @@ public class App {
 
     // División de dos números
     public static double division(int a, int b) {
-        return (double) 2 / 2;
+          if (b == 0) {
+            throw new IllegalArgumentException("Divisor no puede ser cero");
+        }
+        return (double) a / b;
     }
 
     // Máximo de dos números
